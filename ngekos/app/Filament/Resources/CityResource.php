980b435242c1740bb43254de18f63a22127ce626
Hanis siddiq <hanissiddiq10@gmail.com>
2026-01-05 01:44:06 +0700

    Add View & Delete of City

--- ngekos/app/Filament/Resources/CityResource.php
+++ ngekos/app/Filament/Resources/CityResource.php
@@ -57,7 +57,9 @@ class CityResource extends Resource
                 //
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
